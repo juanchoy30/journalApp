a9c5b0a9cfee4c50c7b3b9f625bef8fd8804bebd
Juan Miguel Choy <juanchoy30@gmail.com>
2021-03-26 12:49:56 -0300

    224.Estructura y diseño del HomeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,30 @@ export const NoteScreen = () => {
         <div className="notes__main-content">
 
             <NotesAppBar />
+
+            <div className="notes__content">
+
+                <input
+                    type="text"
+                    placeholder="Type in a title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today?"
+                    className="notes__textarea"
+                >
+                </textarea>
+
+                <div className="notes_image">
+                    <img
+                        src="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270"
+                        alt="Journal"
+                    />
+                </div>
+
+            </div>
             
         </div>
     )
