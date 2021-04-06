a7f8a207dff8ba312d35f5bc36f63d4e56e5949c
Juan Miguel Choy <juanchoy30@gmail.com>
2021-04-06 17:07:50 -0300

    248.Tarea Loading State

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,17 +1,20 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase-config'
 import { types } from "../types/types";
+import { finishLoading, startLoading } from './ui';
 
 export const startLoginEmailPassword = ( email, password ) => {
     return ( dispatch ) => {   // callback because is an asyncronous task
 
+        dispatch( startLoading() );
+
         firebase.auth().signInWithEmailAndPassword( email, password )
             .then( ({ user }) => {
-                dispatch(
-                    login( user.uid, user.displayName )
-                )
+                dispatch( login( user.uid, user.displayName ) );
+                dispatch( finishLoading() );
             })
             .catch( e => {
                 console.log(e);
+                dispatch( finishLoading() );
             });
         
     }
--- src/types/types.js
+++ src/types/types.js
@@ -5,6 +5,9 @@ export const types = {
     logout: '[Auth] Logout',
 
     uiSetError: '[UI] Set Error',
-    uiRemoveError: '[UI] Remove Error'
+    uiRemoveError: '[UI] Remove Error',
 
-}
+    uiStartLoading: '[UI] Start loading',
+    uiFinishLoading: '[UI] Finish loading'
+
+};
